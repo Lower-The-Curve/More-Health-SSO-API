c808f4bfba7ab0050cb1c7e1275de5b5afb4f499
vesapaci1 <vesa.paci@lowerthecurve.com>
2026-06-19 10:32:17 +0200

    remove build artifacts and add .gitignore

--- lib/db/dist/schema/index.d.ts
+++
@@ -1,8 +0,0 @@
-export * from "./userProfile";
-export * from "./address";
-export * from "./order";
-export * from "./activity";
-export * from "./appSettings";
-export * from "./storefrontVisit";
-export * from "./translationOverride";
-//# sourceMappingURL=index.d.ts.map
